32ee07e1392d3eddb1e1238042985e067d2eea31
BrianDYKim <18k7102dy@naver.com>
2022-09-09 23:06:16 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/armeriagrpcexample/ArmeriaGrpcExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.armeriagrpcexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArmeriaGrpcExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<ArmeriaGrpcExampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/armeriagrpcexample/ArmeriaGrpcExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.armeriagrpcexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArmeriaGrpcExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
